31865c65499ad55f2f65d4142de602a483070ebc
Kari Marttila <kari.marttila@iki.fi>
2022-12-23 16:02:27 +0200

    Typescript/React/Vite/Tailwind skeleton using akx starter project is ready

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const demo = (
+  <div className="App">
+    <h1 className="text-3xl font-bold">
+      Hello
+    </h1>
+    <h2>Yeah, uuh, aah, wow, wow!</h2>
+  </div>
+);
+
+
+function App() {
+  return demo;
+}
+
+export default App;
