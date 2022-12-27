9c42e5a132d48f592faa6876b1eea685d1dc8f5c
Kari Marttila <kari.marttila@iki.fi>
2022-12-27 14:18:50 +0200

    Added react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!); // eslint-disable-line
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
