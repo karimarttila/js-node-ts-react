d5295fece4d147e3d9487aec5c270cca70f7e68c
Kari Marttila <kari.marttila@iki.fi>
2023-02-01 20:33:54 +0200

    More integration tests

--- backend/test-integration/routing/router.test.mjs
+++ backend/test-integration/routing/router.test.mjs
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import pkg from 'pactum';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+import { PutFunctionConcurrencyRequestFilterSensitiveLog } from '@aws-sdk/client-lambda';
 
 const { spec } = pkg;
 
@@ -8,7 +11,7 @@ export const port = '6600';
 export const baseUrl = `http://${hostName}:${port}`;
 
 // eslint-disable-next-line no-undef
-it('Call hello', async () => {
+it('Call /hello', async () => {
   await spec()
     .get(`${baseUrl}/hello`)
     .expectStatus(200)
@@ -16,7 +19,7 @@ it('Call hello', async () => {
 });
 
 // eslint-disable-next-line no-undef
-it('Call login', async () => {
+it('Call /login', async () => {
   await spec()
     .post(`${baseUrl}/login`)
     .withHeaders({ 'Content-Type': 'application/json' })
@@ -27,3 +30,20 @@ it('Call login', async () => {
     .expectStatus(200)
     .expectJsonLike({ ret: 'ok' });
 });
+
+it('Call /product-groups', async () => {
+  const res = await axios.post(`${baseUrl}/login`, { username: 'jarska', password: 'joo' });
+  const { token } = res.data;
+  await spec()
+    .get(`${baseUrl}/product-groups`)
+    .withHeaders({ 'x-token': token })
+    .expectStatus(200)
+    .expectJsonMatch({ ret: 'ok', product_groups: [{ pgId: 1, name: 'Books' }, { pgId: 2, name: 'Movies' }] });
+});
+
+// TODO: kirjoita testit:
+// - header x-token puuttuu => 404
+// - /products/:pgId
+// - /product/:pgId/:pId
+// Sitten kirjoita BLOGI backend testauksesta.
+// Sitten toteuta login frontendiin ja kirjoita siitä blogi.
